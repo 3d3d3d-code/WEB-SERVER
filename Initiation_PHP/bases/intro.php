8c37d281bff74c5f2e753a61ea62d7c78e5a619c
unknown <reda.hamza8@orange.fr>
2020-10-26 12:07:44 +0100

    initiation php

---
+++ Initiation_PHP/bases/intro.php
@@ -0,0 +1,76 @@
+<?php
+
+$prenom ="Reda"
+$age = 78;
+
+echo'Hello '.$prenom;
+
+echo'<br>';
+echo "as tu $age ans?" ;
+echo<'<br>';
+echo'oui ';
+
+
+// Structure conditionelles
+
+// if else
+
+if($age>=78){
+
+    echo'vous etes trop ages';
+}
+
+elseif($age<3){
+
+    echo'vous etes un bebe';
+}
+else{
+    echo'on n en sait rien';
+}
+echo'<br>';
+
+
+// switch case break
+
+switch($age)
+{
+    case 3:
+        echo'un bebe';
+    break;
+    case 78:
+        case 89:
+            echo'c\'est pas trop vieux';
+        break;
+}
+echo'<br>';
+
+
+// boucle while et 
+
+$i =o;
+while ($i < 10) {
+  echo"i=$i <br>";
+  $i++;}
+
+  echo'<br>';
+
+
+//boucle do while 
+
+  $i=0;
+
+do{
+
+    echo "i2 = $i <br>";
+    $i++;
+}
+while($i<10)
+
+
+for($i=o;$i<10;$i++){
+
+    echo"i3=$i <br>"
+}
+echo'<br>';
+
+
