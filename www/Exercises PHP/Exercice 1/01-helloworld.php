cf74118188411b6c80bcef42fa9dbf92a52bf065
unknown <reda.hamza8@orange.fr>
2020-10-28 07:54:03 +0100

    commit www

---
+++ www/Exercises PHP/Exercice 1/01-helloworld.php
@@ -0,0 +1,6 @@
+<?php
+
+echo("« HelloWorld")
+
+
+?>
---
+++ www/users_exo.php
@@ -0,0 +1,46 @@
+<html>
+
+<head>
+
+
+
+
+<meta charset="utf-8">
+<title>Users</title>
+
+
+
+
+
+</head>
+
+<body>
+    
+<h1>Utilisateurs</h1>
+
+<?php
+// data source name = chaine de connexion
+
+$dsn ='mysql:host=localhost;port=3306;dbname=bd_users_simple;charset=utf8';
+
+
+/**@var PDO $dbConnect */
+$dbConnect = new PDO($dsn,'root','');
+
+
+/**CONSIGNES :
+ * 
+ * sI LE PARAMETRE 'id' est fourni dans l 'url :
+ * Afficher l utilisateur correspondant a l id <i class="afficher 'aucun resultat si aucun utilisateur n est trouve
+ * 
+ * Sinon 
+ * afficher tous les utilisateurs
+ * 
+ */
+
+
+?>
+
+</body>
+
+</html>
